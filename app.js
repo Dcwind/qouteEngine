98f69b62f4740597bcac13a9de578d0718733afe
DcWind <danielekwuazi@gmail.com>
2016-10-24 23:19:10 +0700

    commit

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 $("#random").click(function() {
   random = "#" + Math.floor(Math.random()*16777215).toString(16);
   $("body").css("background-color", random);
- 
+
 var forismatic = "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?"; //Link of the API
-	
+
 $.getJSON(forismatic, function(data) {
 	$("p").html("\"" + data.quoteText+ "\"" + ' <a id="link" href="" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a>');
 	$("#link").attr("href", "https://twitter.com/intent/tweet?url=[your%20URL]&text=" + '"' + data.quoteText + '"' + " – " + data.quoteAuthor);
@@ -14,5 +14,6 @@ $.getJSON(forismatic, function(data) {
 	$("#author").html("Anonymous");
 	 }
 });
-	
+
 });
+//if no author use Anonymous
